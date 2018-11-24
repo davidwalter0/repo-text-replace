bd65e0e01dc8a999735e6db886e0ff7636fae86a
David Walter <walter.david@gmail.com>
2018-11-24 11:42:56 -0500

    add repo-rm-file to erase a file from git history

--- README.md
+++ README.md
@@ -21,12 +21,16 @@ Process to use and test
 2. Backup your repo
 3. Backup your repo
 4. Did you backup your repo?
-5. edit the key values for the hash map `SEARCH_REPLACE_MAP` in kvmap.sh
+5. edit the key values for the hash map `SEARCH_REPLACE_MAP` or `FILE_REMOVE_MAP` in kvmap.sh
 6. test on the backup
-7. *** touch .${0}.ready
-8. run with real mapping changes
+7. *** touch .${0}.ready # for example `touch .repo-rm-file.ready` for the file remove option
+8. run with real mapping changes with the --exec option
 9. finalize changes test deployments, git push and whatever
 10. git gc
+11. remove the touch safety file(s)
+```
+rm -f .repo-rm-file.ready .repo-text-replace.ready
+```
 
 *When DRY_RUN is 1, try to do no harm, print commands*
 
@@ -47,4 +51,19 @@ file and running with the `--exec` flag
 ```
     touch .repo-text-replace.ready
     repo-text-replace --exec
+```
+
+After backing up your repository, editing the kvmap.sh you can test
+with the `--dry-run` flag
+
+```
+    repo-rm-file --dry-run
+```
+
+After testing you can verify that you are ready by touching the ready
+file and running with the `--exec` flag
+
+```
+    touch .repo-rm-file.ready
+    repo-rm-file --exec
 ```
--- kvmap.sh
+++ kvmap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 declare -A SEARCH_REPLACE_MAP
+declare -A FILE_REMOVE_MAP
 function setup-kv-map
 {
     f1="search-1"
@@ -8,8 +9,18 @@ function setup-kv-map
     f2="search-2"
     r2="replace-2"
     SEARCH_REPLACE_MAP=(
-        [${f1}]=${r1}
+        ["${f1}"]=${r1}
         [${f2}]=${r2}
+    )
+}
+
+function setup-file-map
+{
+    f1="go.mod"
+    f2="go.sum"
+    FILE_REMOVE_MAP=(
+        ["${f1}"]=""
+        ["${f2}"]=""
     )
 }
 
@@ -29,6 +40,27 @@ function list-kv-map
 
     for key in "${!KV_MAP[@]}"; do
         printf "| %-32.32s| %-32.32s|\n" "${key}" "${KV_MAP[${key}]}"
+    done
+    echo "+-------------------------------------------------------------------+"
+}
+
+
+function list-file-kv-map
+{
+    if (( ! ${#} )); then
+        Error1 function list-kv-map: No argument given
+    fi
+    local name="${1}"
+    local -n KV_MAP="${name}"
+    
+    echo "+-------------------------------------------------------------------+"
+    echo "|                   REPO REPAIR FILE DELETIONS                      |"
+    echo "+-------------------------------------------------------------------+"
+    printf "| %-64.64s  |\n" "Remove File"
+    echo "+-------------------------------------------------------------------+"
+
+    for key in "${!KV_MAP[@]}"; do
+        printf "| %-64.64s  |\n" "${key}" 
     done
     echo "+-------------------------------------------------------------------+"
 }
